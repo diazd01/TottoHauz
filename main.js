8fdf7cfe49b98a69a05c485cf22829ed0277bb3d
diazd01 <48544120+diazd01@users.noreply.github.com>
2019-10-22 14:17:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -301,20 +301,12 @@ if (contactForm) {
             contactPhone.style.border = "2px solid #32CD32";
             phoneError.style.display = "none";
         }
-
-        if (document.querySelector('.g-recaptcha').getResponse().length == "") {
-            document.querySelector('.captcha-error').style.display = "block";
-            document.querySelector('.captcha-error').innerHTML = "You must select if you're not a robot."
-        } else {
-            document.querySelector('.captcha-error').style.display = "none";
-        }
         //FINAL VALIDATION: 
         if (nameError.style.display === "none" && emailError.style.display === "none" &&
             phoneError.style.display === "none") {
             contactForm.submit();
-
         }
 
     }
-    console.log(document.querySelector('.data-netlify-recaptcha'));
+
 }
